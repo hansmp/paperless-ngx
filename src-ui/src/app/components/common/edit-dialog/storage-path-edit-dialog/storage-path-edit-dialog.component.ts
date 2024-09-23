b07b8f9a1468881d921c1e873e4479df16ef0d9f
hansmp <46821061+hansmp@users.noreply.github.com>
2024-09-23 20:29:21 +0200

    Adjusted GUI strings for new jinja2 templates

--- src-ui/src/app/components/common/edit-dialog/storage-path-edit-dialog/storage-path-edit-dialog.component.ts
+++ src-ui/src/app/components/common/edit-dialog/storage-path-edit-dialog/storage-path-edit-dialog.component.ts
@@ -26,9 +26,9 @@ export class StoragePathEditDialogComponent extends EditDialogComponent<StorageP
   get pathHint() {
     return (
       $localize`e.g.` +
-      ' <code>{created_year}-{title}</code> ' +
+      ' <code>{{ created_year }}-{{ title }}</code> ' +
       $localize`or use slashes to add directories e.g.` +
-      ' <code>{created_year}/{correspondent}/{title}</code>. ' +
+      ' <code>{{ created_year }}/{{ correspondent }}/{{ title }}</code>. ' +
       $localize`See <a target="_blank" href="https://docs.paperless-ngx.com/advanced_usage/#file-name-handling">documentation</a> for full list.`
     )
   }
